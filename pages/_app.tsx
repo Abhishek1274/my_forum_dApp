31b35a30ff2d91ab255e0c36dbfc0d32b5f00171
Abhishek <121667420+Abhishek1274@users.noreply.github.com>
2023-03-27 13:13:41 +0530

    Add files via upload

---
+++ pages/_app.tsx
@@ -0,0 +1,11 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
+
+//[u][url=https://sepolia.etherscan.io/address/0xff25bb6cf1386cf747702d7df804970857a6dab2]Contract[/url][/u]
+
+
+//[u][url=https://bafybeictl3ppf2dobbfuv45e77mbopjecrvigp6vtvlwivu6wvwh2pk5ja.ipfs.w3s.link/post.json]IPFS URL[/url][/u]
